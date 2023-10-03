7b3fb8dbf64ebead4b0fe4253ae498ea77a15b10
Thiem <thiem.huynh0@gmail.com>
2023-10-03 11:26:07 -0400

    init

---
+++ makeTriangle.js
@@ -0,0 +1,22 @@
+// creates a line of * for a given length
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+// your code goes here.  Make sure you call makeLine() in your own code.
+
+function buildTriangle(size){
+    var triStr = ""
+    for(let i = 0; i < size; i++){
+        triStr = triStr + "\n" + makeLine(i)
+    }
+    return triStr;
+}
+
+
+// test your code by uncommenting the following line
+console.log(buildTriangle(10));
