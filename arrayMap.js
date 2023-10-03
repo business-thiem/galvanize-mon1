d267734056961facc32e0ad99406397700648e56
Thiem <thiem.huynh0@gmail.com>
2023-10-03 13:55:27 -0400

    archiving

---
+++ arrayMap.js
@@ -0,0 +1,12 @@
+
+
+// code runs logic over each element in array and returns new array
+var array1 = [1,2,3,4]
+
+var newArray = array1.map(function(element){
+    element = element + 100
+    return element
+})
+
+
+
