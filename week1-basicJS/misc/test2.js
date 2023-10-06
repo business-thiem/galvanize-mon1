9c21825f995250704a24e62c89258f18f030b335
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-06 10:58:22 -0400

    archiving

---
+++ week1-basicJS/misc/test2.js
@@ -0,0 +1,22 @@
+// let stylesOfPizza = ['new york', 'chicago', 'Neopolitan', 'detroit'];
+
+// console.log(stylesOfPizza.length)
+
+// console.log(1+2+3+4)
+
+
+let fruitsArr = ['Apple', 'Banana', 'Orange'];
+
+// fruitsArr.forEach((fruit, index) => {
+//   if (index === 1) {
+//     console.log(fruit);
+//   }
+// });
+
+
+// fruitsArr.shift()
+
+// console.log(fruitsArr)
+
+
+console.log(fruitsArr.indexOf('Orange'))
---
+++ week1-basicJS/misc/test3.js
@@ -0,0 +1,64 @@
+// var obj = {};
+// obj['runs'] = 'fast';
+// obj.jogs = 'slow';
+
+// obj[jogs]
+
+
+// var myDog = {
+//     name:'Frank',
+//     age:9,
+//     drools:true,
+//   }
+
+//   console.log(Object.entries(myDog))
+
+//   var obj = {a:1};
+// var obj2 = {a:1};
+// console.log(obj === obj2)
+
+
+// var myObj = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 52
+// }
+
+
+// var person = {
+//     name: ['Bob', 'Smith'],
+//     age: 32,
+//     gender: 'male',
+//     interests: ['music', 'skiing'],
+//     bio: function() {
+//       alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+//     },
+//     greeting: function() {
+//       alert('Hi! I\'m ' + this.name[0] + '.');
+//     }
+//   };
+
+
+// console.log(person.interests[person.interests.length-1])
+
+
+// var person = {
+//     name: ['Bob', 'Smith'],
+//     age: 32,
+//     gender: 'male',
+//     interests: ['music', 'skiing'],
+//     bio: function() {
+//       alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+//     },
+//     greeting: function() {
+//       alert('Hi! I\'m ' + this.name[0] + '.');
+//     }
+//   };
+
+//   console.log(person.bio())
+
+
+var obj = {
+    key:"value",
+    "irregular key": 5
+  }
