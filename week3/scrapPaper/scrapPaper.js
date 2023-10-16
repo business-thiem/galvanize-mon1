380fde2f959048f612f3a6e6e66a42602a6f54ec
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-16 16:08:46 -0400

    custom shift complete

--- week3/scrapPaper/scrapPaper.js
+++ week3/scrapPaper/scrapPaper.js
@@ -2,15 +2,20 @@ function customShift(arr) {
     // your code here
 
     let temp = arr[0]
-    for(let i = 0; i < arr.length; i++){
+    if(arr.length === 0){
+        return undefined;
+    } else {
+        for(let i = 0; i < arr.length; i++){
         
-        if(arr.length === 0){
-            return undefined;
-        } else {
-            arr[i] = arr[i+1]
+            if(arr.length === 0){
+                return undefined;
+            } else {
+                arr[i] = arr[i+1]
+            }
+            
         }
-        
     }
+    
 
     arr.length = arr.length - 1
     return temp;
