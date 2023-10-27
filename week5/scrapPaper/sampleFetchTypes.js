a311f162b7e51131fed00b729e68eb42874c403b
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-27 17:14:03 -0400

    testing fetch code

---
+++ week5/scrapPaper/sampleFetchTypes.js
@@ -0,0 +1,39 @@
+
+
+let url = `https://jsonplaceholder.typicode.com/todos`
+
+
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+// ---------------------------------------
+// fetch(url).then(function(res){
+//     return data = res.json()
+// }).then(function(data){
+//     console.log(data)
+// })
+
+
+// ASYNC Await is newer and better
+async function fetchAsyncData() {
+    let data;
+
+    try {
+        let response = await fetch(url)
+        data = await response.json();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  // Calling the async function
+  fetchAsyncData();
+  
+  
+  
+  
+  
+
+
+// console.log(data)
