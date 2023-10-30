785f8f98bf6e17963b583c5a96e7964acb72aae6
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-30 10:35:28 -0400

    playing around with Google Books API

--- week5/scrapPaper/scrapPaper.js
+++ week5/scrapPaper/scrapPaper.js
@@ -1,39 +1,64 @@
+// https://www.googleapis.com/books/v1/volumes?q=intitle:${searchText}
 
 
-let url = `https://jsonplaceholder.typicode.com/todos`
-
-
-// fetch(url)
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-
-// ---------------------------------------
-// fetch(url).then(function(res){
-//     return data = res.json()
-// }).then(function(data){
-//     console.log(data)
-// })
-
-
-// ASYNC Await is newer and better
-async function fetchAsyncData() {
-    let data;
-
-    try {
-        let response = await fetch(url)
-        data = await response.json();
-    } catch (error) {
-        console.error('Error:', error);
-    }
-  }
-  
-  // Calling the async function
-  fetchAsyncData();
-  
-  
-  
-// console.log(data)
+let jsonData;
 
 
 
-// ask garrett by pass by reference and pass by value
+initEventListeners()
+
+// Utility functions ---------------------------------------------
+async function getBookData(searchText){ 
+    let bookapi = `https://www.googleapis.com/books/v1/volumes?q=intitle:${searchText}`
+    let res = await fetch(bookapi);
+    jsonData = await res.json();
+    console.log(jsonData)
+
+    renderBooks(jsonData)
+}
+
+function initEventListeners(){
+    const userSearch = document.querySelector(`#userSearch`)
+    const searchBtn = document.querySelector(`#searchBtn`)
+    // using enterbtn
+    userSearch.addEventListener('keyup', (e) => {
+        if(e.keyCode === 13){
+            getBookData(userSearch.value)
+        }
+    })
+    searchBtn.addEventListener('click', (e) => {
+        getBookData(userSearch.value)
+    })
+}
+
+
+function renderBooks(obj){
+    const dataCtn = document.querySelector(`#dataCtn`)
+    const books = obj.items;
+    let bookHTML = ``
+
+    //empty contents
+    dataCtn.innerHTML = '';
+
+    // obj.volumeInfo.imageLinks.thumbnail
+    let volumeInfo = '';
+
+    books.forEach((book,index) => {
+        
+        volumeInfo = book.volumeInfo;
+
+        // console.log(book.volumeInfo.imageLinks.thumbnail)
+
+        bookHTML += `
+            <img src="${volumeInfo.imageLinks.thumbnail}">
+            <h4>${volumeInfo.title}</h4>
+            <p>${volumeInfo.description}</p>
+            `
+    })
+    
+    dataCtn.innerHTML = bookHTML;
+
+    // dataCtn.append(bookHTML)
+}
+
+
