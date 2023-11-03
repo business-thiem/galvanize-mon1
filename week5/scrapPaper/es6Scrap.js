b61e2b9b1a173f8c9773d38eb611dc5176bcc10c
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-03 10:13:46 -0400

    archiving es6 class constructor

---
+++ week5/scrapPaper/es6Scrap.js
@@ -0,0 +1,21 @@
+//Not finished started this thinking it was a small project
+
+class ViewTracker {
+
+    constructor(show) {
+        this.show = show
+    }
+    
+    getShow(show){
+        return this.show
+    }
+    
+    trackView = async (views) => {
+        getShow()
+    }
+    
+    summary(show){
+        
+    }
+
+}
