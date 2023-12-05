2cbb0529e72d757c5cf9a41b1723e0d224397f7e
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-05 13:27:23 -0500

    scrap update

--- week7-liveserver-auth/sampleTest1/test.sql
+++ week7-liveserver-auth/sampleTest1/test.sql
@@ -3,6 +3,10 @@ FROM people
 INNER JOIN companies ON people.company_id = companies.id
 WHERE people.company_id IS NOT NULL;
 
+-- SELECT people.first_name, companies.name
+-- FROM people
+-- WHERE company_id IS NOT NULL;
+
 
 -- SELECT ProductID, ProductName, CategoryName
 -- FROM Products
