cffe06cb25f1d731ada07930f11b10ccbc60f8d9
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-05 16:10:59 -0500

    refactor

--- week9 - OOP/getter&setter/printer2.js
+++ week9 - OOP/getter&setter/printer2.js
@@ -8,7 +8,7 @@ const Printer = (name, sheetCount = 0) => {
             return sheetCount
         },
         addSheets(numSheets) {
-            sheetCount = sheetCount + numSheets;
+            sheetCount += numSheets;
         },
         printJob(name, size) {
             if(size > this.sheetCount) throw new Error("Job failed: please refill paper tray!");
@@ -16,19 +16,22 @@ const Printer = (name, sheetCount = 0) => {
             for(let i = 1; i <= size; i++) {
             console.log(`Printing ${name} - page ${i} of ${size}`);
             }
-            sheetCount = sheetCount - size;
+            sheetCount -= size;
         }
     }
   };
 
 
 const p1 = Printer("Canon", 5)
-
 console.log(p1)
-console.log(p1.name)
 
 p1.name = "Nikon" // doesn't change name
 
 console.log(p1.name)
 
 p1.printJob("myPhotos", 4)
+console.log(p1.sheetCount)
+p1.addSheets(30);
+console.log(p1.sheetCount)
+p1.printJob("myPhotos2", 2)
+console.log(p1.sheetCount)
